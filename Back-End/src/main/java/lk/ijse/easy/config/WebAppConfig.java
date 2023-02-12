0bda26fb46d78ac5ce4556cee16197d20d7de6b5
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-12 17:04:53 +0530

    Added WebAppConfig Class in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/config/WebAppConfig.java
@@ -0,0 +1,17 @@
+package lk.ijse.easy.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author : ShEnUx
+ * @time : 5:03 PM
+ * @date : 2/12/2023
+ * @since : 0.1.0
+ **/
+@Configuration
+@EnableWebMvc
+@ComponentScan()
+public class WebAppConfig {
+}
