97017d6602c862715e757ce92d94459a4344573e
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-12 17:07:39 +0530

    Added WebRootConfig Class in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/config/WebRootConfig.java
@@ -0,0 +1,16 @@
+package lk.ijse.easy.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @author : ShEnUx
+ * @time : 5:06 PM
+ * @date : 2/12/2023
+ * @since : 0.1.0
+ **/
+@Configuration
+@ComponentScan()
+public class WebRootConfig {
+}
