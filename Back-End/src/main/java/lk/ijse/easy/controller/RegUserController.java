be5dbd77511be2f97ac0e67a67be2c0dc30e2065
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-13 16:59:47 +0530

    Added Save Method to RegUserController in the Back-End

--- Back-End/src/main/java/lk/ijse/easy/controller/RegUserController.java
+++ Back-End/src/main/java/lk/ijse/easy/controller/RegUserController.java
@@ -1,8 +1,10 @@
 package lk.ijse.easy.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.easy.dto.RegUserDTO;
+import lk.ijse.easy.service.RegUserService;
+import lk.ijse.easy.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author : ShEnUx
@@ -14,4 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("/customer")
 public class RegUserController {
+    @Autowired
+    private RegUserService service;
+    @PostMapping
+    public ResponseUtil saveRegUser(@ModelAttribute RegUserDTO regUserDTO) {
+        service.saveRegUser(regUserDTO);
+        return new ResponseUtil("OK", "Successfully Registered..!", null);
+    }
 }
