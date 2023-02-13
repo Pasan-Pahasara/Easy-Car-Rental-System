cdce32c0178e4c3b5d786109ecb5a524da4f211b
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-13 23:09:10 +0530

    Refactored Delete Method to DriverServiceImpl in the Back-End

--- Back-End/src/main/java/lk/ijse/easy/service/impl/DriverServiceImpl.java
+++ Back-End/src/main/java/lk/ijse/easy/service/impl/DriverServiceImpl.java
@@ -34,7 +34,10 @@ public class DriverServiceImpl implements DriverService {
     }
 
     public void deleteDriver(String id) {
-
+        if (!repo.existsById(id)) {
+            throw new RuntimeException("Wrong ID..Please enter valid id..!");
+        }
+        repo.deleteById(id);
     }
 
     public void updateDriver(DriverDTO driverDTO) {
