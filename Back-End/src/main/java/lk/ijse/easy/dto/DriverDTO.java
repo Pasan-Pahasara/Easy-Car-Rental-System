ec8b6d4e9c08e6689187651d22d3d4fb10f09091
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-13 22:39:47 +0530

    Introducing DriverDTO in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/dto/DriverDTO.java
@@ -0,0 +1,27 @@
+package lk.ijse.easy.dto;
+
+import lk.ijse.easy.embeded.Name;
+import lombok.Data;
+
+import javax.persistence.Embedded;
+
+/**
+ * @author : ShEnUx
+ * @time : 10:38 PM
+ * @date : 2/13/2023
+ * @since : 0.1.0
+ **/
+@Data
+public class DriverDTO {
+    private String driver_Id;
+    private String role_Type;
+    private Name name;
+    private String contact_No;
+    private String address;
+    private String email;
+    private String nic;
+    private String license_No;
+    private String password;
+    private String nic_Img;
+    private String license_Img;
+}
