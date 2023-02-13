08c6c0027f716793f4602b9725aeeb668592ec27
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-13 15:20:22 +0530

    Introducing CustomerDTO in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/dto/CustomerDTO.java
@@ -0,0 +1,35 @@
+package lk.ijse.easy.dto;
+
+import lk.ijse.easy.embeded.Name;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embedded;
+
+
+/**
+ * @author : ShEnUx
+ * @time : 3:17 PM
+ * @date : 2/13/2023
+ * @since : 0.1.0
+ **/
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class CustomerDTO {
+    private String user_Id;
+    private String role_Type;
+    @Embedded
+    private Name name;
+    private String contact_No;
+    private String address;
+    private String email;
+    private String nic;
+    private String license_No;
+    private String password;
+    private String nic_Img;
+    private String license_Img;
+}
