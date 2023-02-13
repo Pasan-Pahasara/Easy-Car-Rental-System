e5d2c81c11ad0010ef38dd950dcea5714b43ca5f
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-13 16:54:41 +0530

    FullyRefactored RegUserServiceImpl in the Back-End

--- Back-End/src/main/java/lk/ijse/easy/service/impl/RegUserServiceImpl.java
+++ Back-End/src/main/java/lk/ijse/easy/service/impl/RegUserServiceImpl.java
@@ -1,7 +1,12 @@
 package lk.ijse.easy.service.impl;
 
 import lk.ijse.easy.dto.RegUserDTO;
+import lk.ijse.easy.entity.RegUser;
+import lk.ijse.easy.repo.RegUserRepo;
 import lk.ijse.easy.service.RegUserService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -16,19 +21,33 @@ import java.util.ArrayList;
 @Service
 @Transactional
 public class RegUserServiceImpl implements RegUserService {
-    public void saveRegUser(RegUserDTO regUserDTO) {
+    @Autowired
+    private RegUserRepo repo;
+    @Autowired
+    private ModelMapper mapper;
 
+    public void saveRegUser(RegUserDTO regUserDTO) {
+        if (repo.existsById(regUserDTO.getUser_Id())) {
+            throw new RuntimeException("User Already Exist. Please enter another id..!");
+        }
+        repo.save(mapper.map(regUserDTO, RegUser.class));
     }
 
     public void deleteRegUser(String id) {
-
+        if (!repo.existsById(id)) {
+            throw new RuntimeException("Wrong ID..Please enter valid id..!");
+        }
+        repo.deleteById(id);
     }
 
     public void updateRegUser(RegUserDTO regUserDTO) {
-
+        if (!repo.existsById(regUserDTO.getUser_Id())) {
+            throw new RuntimeException("Wrong ID..No Such a User to Update..!");
+        }
+        repo.save(mapper.map(regUserDTO, RegUser.class));
     }
 
     public ArrayList<RegUserDTO> getAllRegUsers() {
-        return null;
+        return mapper.map(repo.findAll(), new TypeToken<ArrayList<RegUserDTO>>() {}.getType());
     }
 }
