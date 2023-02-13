3d7bd2164fc9643fa9302a245cb4295ae3459182
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-13 22:43:25 +0530

    Introducing DriverController in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/controller/DriverController.java
@@ -0,0 +1,17 @@
+package lk.ijse.easy.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : ShEnUx
+ * @time : 10:42 PM
+ * @date : 2/13/2023
+ * @since : 0.1.0
+ **/
+@RestController
+@CrossOrigin
+@RequestMapping("/driver")
+public class DriverController {
+}
