e2c0b1bb97e44394d2df0de3045b4b47a7de1cd5
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-13 15:34:41 +0530

    Introducing RegUserService and Refactored it in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/service/RegUserService.java
@@ -0,0 +1,18 @@
+package lk.ijse.easy.service;
+
+import lk.ijse.easy.dto.RegUserDTO;
+
+import java.util.ArrayList;
+
+/**
+ * @author : ShEnUx
+ * @time : 3:23 PM
+ * @date : 2/13/2023
+ * @since : 0.1.0
+ **/
+public interface RegUserService {
+    void saveRegUser(RegUserDTO regUserDTO);
+    void deleteRegUser(String id);
+    void updateRegUser(RegUserDTO regUserDTO);
+    ArrayList<RegUserDTO> getAllRegUsers();
+}
