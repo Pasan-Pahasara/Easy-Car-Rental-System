91bb5d70e0663436f0f85272566d42529e94fe2c
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-13 15:08:54 +0530

    Introducing CustomerController in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package lk.ijse.easy.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : ShEnUx
+ * @time : 3:07 PM
+ * @date : 2/13/2023
+ * @since : 0.1.0
+ **/
+@RestController
+@CrossOrigin
+@RequestMapping("/customer")
+public class CustomerController {
+}
