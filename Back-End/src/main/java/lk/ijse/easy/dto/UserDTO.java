93d2987ec5e8265ff4421ef107328405975c2ce1
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-14 13:17:08 +0530

    Added UserDTO in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/dto/UserDTO.java
@@ -0,0 +1,26 @@
+package lk.ijse.easy.dto;
+
+import lk.ijse.easy.enums.RoleType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : ShEnUx
+ * @time : 1:08 PM
+ * @date : 2/14/2023
+ * @since : 0.1.0
+ **/
+@Data
+@NoArgsConstructor
+public class UserDTO {
+    private String user_Id;
+    private RoleType role_Type;
+    private String user_Name;
+    private String password;
+
+    public UserDTO(RoleType role_Type, String user_Name, String password) {
+        this.role_Type = role_Type;
+        this.user_Name = user_Name;
+        this.password = password;
+    }
+}
