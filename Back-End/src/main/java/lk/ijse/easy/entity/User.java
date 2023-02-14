d2d6b6d8097d973076d86e3b36b1da885fa50d72
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-14 13:04:21 +0530

    Added User Entity in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/entity/User.java
@@ -0,0 +1,32 @@
+package lk.ijse.easy.entity;
+
+import lk.ijse.easy.enums.RoleType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * @author : ShEnUx
+ * @time : 12:40 PM
+ * @date : 2/14/2023
+ * @since : 0.1.0
+ **/
+@Data
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String user_Id;
+    @Enumerated(EnumType.STRING)
+    private RoleType role_Type;
+    private String user_Name;
+    private String password;
+
+    public User(RoleType role_Type, String user_Name, String password) {
+        this.role_Type = role_Type;
+        this.user_Name = user_Name;
+        this.password = password;
+    }
+}
