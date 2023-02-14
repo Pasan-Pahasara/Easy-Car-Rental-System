adb6f45b56e4c5e78207eee2243b3a6657594fcd
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-14 10:13:35 +0530

    Refactored Save Method to AdminServiceImpl in the Back-End

--- Back-End/src/main/java/lk/ijse/easy/controller/AdminController.java
+++ Back-End/src/main/java/lk/ijse/easy/controller/AdminController.java
@@ -1,5 +1,7 @@
 package lk.ijse.easy.controller;
 
+import lk.ijse.easy.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ Back-End/src/main/java/lk/ijse/easy/service/impl/AdminServiceImpl.java
@@ -0,0 +1,47 @@
+package lk.ijse.easy.service.impl;
+
+/**
+ * @author : ShEnUx
+ * @time : 9:24 AM
+ * @date : 2/14/2023
+ * @since : 0.1.0
+ **/
+
+import lk.ijse.easy.dto.AdminDTO;
+import lk.ijse.easy.entity.Admin;
+import lk.ijse.easy.entity.Driver;
+import lk.ijse.easy.repo.AdminRepo;
+import lk.ijse.easy.service.AdminService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+@Service
+@Transactional
+public class AdminServiceImpl implements AdminService {
+    @Autowired
+    private AdminRepo repo;
+    @Autowired
+    private ModelMapper mapper;
+
+    public void saveAdmin(AdminDTO adminDTO) {
+        if (repo.existsById(adminDTO.getAdmin_Id())) {
+            throw new RuntimeException("User Already Exist. Please enter another id..!");
+        }
+        repo.save(mapper.map(adminDTO, Admin.class));
+    }
+
+    public void deleteAdmin(String id) {
+
+    }
+
+    public void updateAdmin(AdminDTO adminDTO) {
+
+    }
+
+    public ArrayList<AdminDTO> getAdmin() {
+        return null;
+    }
+}
