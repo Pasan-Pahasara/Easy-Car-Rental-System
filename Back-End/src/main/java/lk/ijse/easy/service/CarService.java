0009b76f71bf30fff1664419a5a0a4436b95dc4f
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-14 10:44:36 +0530

    Introducing CarService in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/service/CarService.java
@@ -0,0 +1,19 @@
+package lk.ijse.easy.service;
+
+import lk.ijse.easy.dto.AdminDTO;
+import lk.ijse.easy.dto.CarDTO;
+
+import java.util.ArrayList;
+
+/**
+ * @author : ShEnUx
+ * @time : 10:43 AM
+ * @date : 2/14/2023
+ * @since : 0.1.0
+ **/
+public interface CarService {
+    void saveCar(CarDTO carDTO);
+    void deleteCar(String id);
+    void updateCar(CarDTO carDTO);
+    ArrayList<CarDTO> getAllCars();
+}
