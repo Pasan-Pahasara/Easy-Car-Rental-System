5a055523b431a67a6a99504d84963c8dfd803ca9
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-14 09:10:26 +0530

    Introducing AdminController in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/controller/AdminController.java
@@ -0,0 +1,17 @@
+package lk.ijse.easy.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : ShEnUx
+ * @time : 9:08 AM
+ * @date : 2/14/2023
+ * @since : 0.1.0
+ **/
+@RestController
+@CrossOrigin
+@RequestMapping("/admin")
+public class AdminController {
+}
