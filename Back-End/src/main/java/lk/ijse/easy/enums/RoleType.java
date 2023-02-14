1b460a4f2343040899a3d656a8c1ecd88c8b7875
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-14 13:03:41 +0530

    Added RoleType Enum in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/enums/RoleType.java
@@ -0,0 +1,11 @@
+package lk.ijse.easy.enums;
+
+/**
+ * @author : ShEnUx
+ * @time : 1:00 PM
+ * @date : 2/14/2023
+ * @since : 0.1.0
+ **/
+public enum RoleType {
+    ADMIN,DRIVER,REGISTERED_USER
+}
