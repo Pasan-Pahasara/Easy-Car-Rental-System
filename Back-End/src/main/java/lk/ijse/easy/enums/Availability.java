0ae1dead74524144046da494e101052ada774e64
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-14 13:49:52 +0530

    Added Availability Enum in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/enums/Availability.java
@@ -0,0 +1,11 @@
+package lk.ijse.easy.enums;
+
+/**
+ * @author : ShEnUx
+ * @time : 1:47 PM
+ * @date : 2/14/2023
+ * @since : 0.1.0
+ **/
+public enum Availability {
+    AVAILABLE,UNAVAILABLE,
+}
