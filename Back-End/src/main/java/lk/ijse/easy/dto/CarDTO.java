171b10a8e33488c59dcf84fad85bb639845f93f3
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-14 10:39:18 +0530

    Introducing CarDTO in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/dto/CarDTO.java
@@ -0,0 +1,33 @@
+package lk.ijse.easy.dto;
+
+import lk.ijse.easy.embeded.Image;
+import lk.ijse.easy.embeded.Rate;
+import lombok.Data;
+
+import javax.persistence.Embedded;
+import javax.persistence.Id;
+
+/**
+ * @author : ShEnUx
+ * @time : 10:37 AM
+ * @date : 2/14/2023
+ * @since : 0.1.0
+ **/
+@Data
+public class CarDTO {
+    private String car_Id;
+    private String name;
+    private String brand;
+    private String type;
+    @Embedded
+    private Image image;
+    private int number_Of_Passengers;
+    private String transmission_Type;
+    private String fuel_Type;
+    @Embedded
+    private Rate rent_Duration_Price;
+    private String price_Extra_KM;
+    private  String registration_Number;
+    private int car_qty;
+    private String color;
+}
