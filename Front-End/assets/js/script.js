106e9d1013c043de0b7a1f985cc42cb88d03069f
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-17 01:45:36 +0530

    Added Reviews Section Swiper JS and Css in the Front-End

---
+++ Front-End/assets/js/script.js
@@ -0,0 +1,40 @@
+/**
+ * @author : Pasan Pahasara
+ * @since : 0.1.0
+ **/
+
+/**
+ * Reviews Section Swiper Start
+ */
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+});
+/**
+ * Reviews Section Swiper End
+ */
