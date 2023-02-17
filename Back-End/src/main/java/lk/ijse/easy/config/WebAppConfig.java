8349b17bbb5b97ca48362649f3d1f3a4a4ca2f4f
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-17 14:44:26 +0530

    Refactored WebAppConfig in the Back-End

--- Back-End/src/main/java/lk/ijse/easy/config/WebAppConfig.java
+++ Back-End/src/main/java/lk/ijse/easy/config/WebAppConfig.java
@@ -1,8 +1,12 @@
 package lk.ijse.easy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author : ShEnUx
@@ -13,5 +17,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"lk.ijse.easy.controller","lk.ijse.easy.advisor"})
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(500000000);
+        return multipartResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**").addResourceLocations("/uploads/");
+    }
 }
