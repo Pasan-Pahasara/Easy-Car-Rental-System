ccff126a08a60eda24e05997942c13726735a126
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-17 15:29:12 +0530

    Update Content

--- Back-End/src/main/java/lk/ijse/easy/config/WebAppConfig.java
+++ Back-End/src/main/java/lk/ijse/easy/config/WebAppConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.easy.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
