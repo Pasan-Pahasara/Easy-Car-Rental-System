fbde9fabecb863841992c93fdb750e80f4c5cb88
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-18 20:34:51 +0530

    Added Rent RentDetail_PK Class in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/entity/RentDetail_PK.java
@@ -0,0 +1,14 @@
+package lk.ijse.easy.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author : ShEnUx
+ * @time : 8:32 PM
+ * @date : 2/18/2023
+ * @since : 0.1.0
+ **/
+public class RentDetail_PK implements Serializable {
+    private String car_Id;
+    private String rent_Id;
+}
