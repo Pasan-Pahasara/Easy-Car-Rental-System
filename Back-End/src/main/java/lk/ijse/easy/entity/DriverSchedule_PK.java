09f054d9cc31ca69f4147d6d33fd099c65aaa056
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-18 21:15:36 +0530

    Added DriverSchedule_PK Class in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/entity/DriverSchedule_PK.java
@@ -0,0 +1,14 @@
+package lk.ijse.easy.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author : ShEnUx
+ * @time : 9:13 PM
+ * @date : 2/18/2023
+ * @since : 0.1.0
+ **/
+public class DriverSchedule_PK implements Serializable {
+    private String driver_Id;
+    private String rent_Id;
+}
