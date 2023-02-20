b2ae32a22540c5ac2d51e464a416fed9af6f8f4f
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-20 10:37:59 +0530

    Added Load All Cars and Refactored Customer JS and adminDashboard in the Back-End

--- Front-End/assets/js/customerJS.js
+++ Front-End/assets/js/customerJS.js
@@ -2,8 +2,9 @@
  * @author : Pasan Pahasara
  * @since : 0.1.0
  **/
-let baseUrl = "http://localhost:8080/Back_End_war/";
+let carBaseUrl = "http://localhost:8080/Back_End_war/";
 <!-- Start Car Section -->
+carLoadTable();
 
 <!-- start car add -->
 $("#addCarBtn").on('click', function () {
@@ -52,7 +53,7 @@ $("#addCarBtn").on('click', function () {
     }
 
     $.ajax({
-        url: baseUrl + "car",
+        url: carBaseUrl + "car",
         method: "post",
         contentType: "application/json",
         data: JSON.stringify(carOb),
@@ -116,7 +117,7 @@ $("#updateCar").on('click', function () {
     }
 
     $.ajax({
-        url: baseUrl + "driver",
+        url: carBaseUrl + "car",
         method: "put",
         contentType: "application/json",
         data: JSON.stringify(carOb),
@@ -131,5 +132,26 @@ $("#updateCar").on('click', function () {
     });
 });
 <!-- end car update -->
+
+<!-- start load car function -->
+function carLoadTable() {
+    $("#tblCar").empty();
+    $.ajax({
+        url: carBaseUrl + "car",
+        method: "get",
+        contentType: "application/json",
+        success: function (resp) {
+            for (let car of resp.data) {
+                let row = `<tr><td>${car.car_Id}</td><td>${car.car_name}</td><td>${car.car_brand}</td><td>${car.type}</td><td>${car.image.front_View}</td><td>${car.image.back_View}</td><td>${car.image.side_View}</td><td>${car.image.interior}</td><td>${car.number_Of_Passengers}</td><td>${car.transmission_Type}</td><td>${car.fuel_Type}</td><td>${car.rent_Duration_Price.daily_Rate}</td><td>${car.rent_Duration_Price.monthly_Rate}</td><td>${car.price_Extra_KM}</td><td>${car.registration_Number}</td><td>${car.free_Mileage}</td><td>${car.color}</td><td>${car.car_Availability}</td></tr>`;
+                $("#tblCar").append(row);
+            }
+        },
+        error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+}
+<!-- end load car function -->
 
 <!-- End Car Section -->
