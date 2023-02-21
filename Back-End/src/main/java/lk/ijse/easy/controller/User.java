67264161ed91d4c97def9394ac4a66ab4c0f101f
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-21 23:21:30 +0530

    Introducing User Controller in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/controller/User.java
@@ -0,0 +1,17 @@
+package lk.ijse.easy.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : ShEnUx
+ * @time : 11:16 PM
+ * @date : 2/21/2023
+ * @since : 0.1.0
+ **/
+@RestController
+@CrossOrigin
+@RequestMapping("/user")
+public class User {
+}
