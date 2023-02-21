0e64ee3e966c8b5666a3592e6817be82b6af1ceb
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-21 23:23:27 +0530

    Introducing UserService in the Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/service/userService.java
@@ -0,0 +1,15 @@
+package lk.ijse.easy.service;
+
+import lk.ijse.easy.dto.UserDTO;
+
+import java.util.ArrayList;
+
+/**
+ * @author : ShEnUx
+ * @time : 11:22 PM
+ * @date : 2/21/2023
+ * @since : 0.1.0
+ **/
+public interface userService {
+    ArrayList<UserDTO> getAllRegUsers();
+}
