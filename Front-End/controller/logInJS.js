73c034b573792f6f0f2903ca235a9faa96c1aa89
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-22 00:01:59 +0530

    FullyRefactored logInJS in the Back-End

---
+++ Front-End/controller/logInJS.js
@@ -0,0 +1,43 @@
+/**
+ * @author : Pasan Pahasara
+ * @since : 0.1.0
+ **/
+
+let loginBaseUrl = "http://localhost:8080/Back_End_war/";
+<!-- Start LogIn Section -->
+
+<!-- start login using login button -->
+$("#loginBtn").on('click', function () {
+    login();
+});
+<!-- end login using login button -->
+
+<!-- start login function -->
+function login() {
+    let loginRole_Type = $("#loginRole_Type").val();
+    let loginUserName = $("#loginUserName").val();
+    let loginPassword = $("#loginPassword").val();
+
+    $.ajax({
+        url: loginBaseUrl + "user",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (res) {
+            for (var login of res.data) {
+                if (loginRole_Type === login.role_Type && loginUserName === login.user_Name && loginPassword === login.password) {
+                    if (loginRole_Type === "DRIVER" && loginUserName === login.user_Name && loginPassword === login.password) {
+                        window.location.href = 'driverDashboard.html';
+                    } else if (loginRole_Type === "REGISTERED_USER" && loginUserName === login.user_Name && loginPassword === login.password) {
+                        window.location.href = 'regUser.html';
+                    } else if (loginRole_Type === "ADMIN" && loginUserName === login.user_Name && loginPassword === login.password) {
+                        window.location.href = 'adminDashboard.html';
+                    }
+                    return;
+                }
+            }
+        }
+    });
+}
+<!-- end login function -->
+
+<!-- End LogIn Section -->
