ee3d4c692c7b5a8aa3f0921cff1ebb8efa2dae91
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-23 15:08:17 +0530

    Added get user nativeQuery and Refactored UserRepo in the Back-End

--- Back-End/src/main/java/lk/ijse/easy/repo/UserRepo.java
+++ Back-End/src/main/java/lk/ijse/easy/repo/UserRepo.java
@@ -13,5 +13,6 @@ import org.springframework.data.jpa.repository.Query;
  **/
 
 public interface UserRepo extends JpaRepository<User,String> {
-
+    @Query(value = "select * from User where user_Name=? AND password=? limit 1",nativeQuery = true)
+    User findUserByUser_NameAndPassword(String username, String password) throws RuntimeException;
 }
