6091c9df8468755370708eb4c9ee05488a163344
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-23 21:05:08 +0530

    Refactored RentJS Set Values Using Car Id in Front-End

--- Front-End/controller/rentJS.js
+++ Front-End/controller/rentJS.js
@@ -31,4 +31,47 @@ $("#fuelType").on('click',function () {
     })
 });
 
+$("#carID").on('click',function () {
+    var search = $("#carID").val();
+    $.ajax({
+        url: rentBaseUrl + "car/searchCar/?car_Id=" + search,
+        method: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (res) {
+            console.log(res);
+            $("#carModel").val(res.car_name);
+            $("#carBrand").val(res.car_brand);
+            $("#noOfPassengers").val(res.number_Of_Passengers);
+            let frontUrl = res.image.front_View;
+            let interiorUrl = res.image.interior;
+            let sideUrl = res.image.side_View;
+            $("#frontImage").css({
+                "background": `url(${rentBaseUrl + frontUrl})`,
+                "background-size": "cover",
+                "height": "11rem",
+                "padding": "1rem",
+                "border-radius": "1rem"
+            });
+            $("#interiorImage").css({
+                "background": `url(${rentBaseUrl + interiorUrl})`,
+                "background-size": "cover",
+                "height": "11rem",
+                "padding": "1rem",
+                "border-radius": "1rem"
+            });
+            $("#sideImage").css({
+                "background": `url(${rentBaseUrl + sideUrl})`,
+                "background-size": "cover",
+                "height": "11rem",
+                "padding": "1rem",
+                "border-radius": "1rem"
+            });
 
+        },
+        error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            console.log(message);
+        }
+    })
+});
