b4ff7ca103672fd37dab9d425285e5436e15a615
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-23 23:54:23 +0530

    Introducing rentService in Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/service/RentService.java
@@ -0,0 +1,14 @@
+package lk.ijse.easy.service;
+
+import lk.ijse.easy.dto.RentDTO;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @author : ShEnUx
+ * @time : 11:53 PM
+ * @date : 2/23/2023
+ * @since : 0.1.0
+ **/
+public interface RentService {
+    void bookingCars(@RequestBody RentDTO dto);
+}
