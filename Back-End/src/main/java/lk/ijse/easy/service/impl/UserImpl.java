8012553ac11cb2ce033a24d8ba59f98d0182f098
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-23 15:08:57 +0530

    FullyRefactored GetRegUser in UserServiceImpl in the Back-End

--- Back-End/src/main/java/lk/ijse/easy/service/impl/UserImpl.java
+++ Back-End/src/main/java/lk/ijse/easy/service/impl/UserImpl.java
@@ -33,6 +33,6 @@ public class UserImpl implements UserService {
 
     @Override
     public UserDTO getRegUsers(String username, String password) {
-       return null;
+       return mapper.map(repo.findUserByUser_NameAndPassword(username,password),UserDTO.class);
     }
 }
