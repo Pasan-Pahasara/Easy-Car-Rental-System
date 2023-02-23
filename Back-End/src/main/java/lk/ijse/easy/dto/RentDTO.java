860dcbc7adf46e85a78fb7c44b414e456e0e052b
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-23 23:20:38 +0530

    Refactored RentDTO and RentDetailsDTO in Back-End

--- Back-End/src/main/java/lk/ijse/easy/dto/RentDTO.java
+++ Back-End/src/main/java/lk/ijse/easy/dto/RentDTO.java
@@ -1,5 +1,13 @@
 package lk.ijse.easy.dto;
 
+import lk.ijse.easy.entity.RegUser;
+import lk.ijse.easy.enums.RentRequestType;
+import lk.ijse.easy.enums.RequestType;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 /**
  * @author : ShEnUx
  * @time : 11:10 PM
@@ -7,4 +15,16 @@ package lk.ijse.easy.dto;
  * @since : 0.1.0
  **/
 public class RentDTO {
+    private String rentID;
+    private LocalDate pickUpDate;
+    private LocalTime pickUpTime;
+    private LocalDate returnDate;
+    private LocalTime returnTime;
+    private RequestType requestType;
+    private RentRequestType rentType;
+    private String location;
+    private RegUser regUser;
+
+//    private List<DriverSchedule> driverSchedules;
+    private List<RentDetailsDTO> rentDetails;
 }
---
+++ Back-End/src/main/java/lk/ijse/easy/dto/RentDetailsDTO.java
@@ -0,0 +1,17 @@
+package lk.ijse.easy.dto;
+
+import lk.ijse.easy.entity.Car;
+import lk.ijse.easy.entity.Rent;
+
+/**
+ * @author : ShEnUx
+ * @time : 11:17 PM
+ * @date : 2/23/2023
+ * @since : 0.1.0
+ **/
+public class RentDetailsDTO {
+    private String carID;
+    private String rentID;
+    private Rent rent;
+    private Car car;
+}
