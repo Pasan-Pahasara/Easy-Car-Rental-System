7ca42eac3d13390c1bb579626bb902963fe403be
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-23 20:31:26 +0530

    CarRepo FullyRefactored in Back-End

--- Back-End/src/main/java/lk/ijse/easy/repo/CarRepo.java
+++ Back-End/src/main/java/lk/ijse/easy/repo/CarRepo.java
@@ -2,6 +2,9 @@ package lk.ijse.easy.repo;
 
 import lk.ijse.easy.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.ArrayList;
 
 /**
  * @author : ShEnUx
@@ -10,4 +13,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @since : 0.1.0
  **/
 public interface CarRepo extends JpaRepository<Car,String> {
+    @Query(value = "SELECT * FROM Car WHERE type =?1 and fuel_Type=?2 and car_Availability='AVAILABLE' ", nativeQuery = true)
+    ArrayList<Car> filterCar(String type, String fuel_Type);
 }
