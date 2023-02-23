b27f0e0cda84e7b3121a9bd91535379c1797962b
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-23 23:50:23 +0530

    Introducing rentController in Back-End

---
+++ Back-End/src/main/java/lk/ijse/easy/controller/rentController.java
@@ -0,0 +1,16 @@
+package lk.ijse.easy.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author : ShEnUx
+ * @time : 11:32 PM
+ * @date : 2/23/2023
+ * @since : 0.1.0
+ **/
+@RestController
+@CrossOrigin
+@RequestMapping("/rent")
+public class rentController {
+
+}
